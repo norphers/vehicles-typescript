2d51f425c24f8650c0240a547a7f7050addd24ce
ocanades <ocanades@gmail.com>
2020-10-21 19:37:00 +0200

    done => fase 3 todo => ¿por qué no desaparece el mensaje de error al volver a hacer submit con los datos correctos?

--- exercise/VehiclesFE/controllers/controller.js
+++ exercise/VehiclesFE/controllers/controller.js
@@ -4,8 +4,11 @@ function createCar() {
     var plate = document.getElementById("inputPlate");
     var brand = document.getElementById("inputBrand");
     var color = document.getElementById("inputColor");
-    car = new Car(plate.value, color.value, brand.value);
-    generateCarDisplay();
+    var carDataValidation = carRegisterValidation(plate, brand, color);
+    if (carDataValidation) {
+        car = new Car(plate.value, color.value, brand.value);
+        generateCarDisplay();
+    }
 }
 function createWheels() {
     var wheel1Brand = document.getElementById("inputWheel1Brand");
@@ -16,16 +19,20 @@ function createWheels() {
     var wheel3Diameter = document.getElementById("inputWheel3Diameter");
     var wheel4Brand = document.getElementById("inputWheel4Brand");
     var wheel4Diameter = document.getElementById("inputWheel4Diameter");
-    var wheel1 = new Wheel(wheel1Diameter.valueAsNumber, wheel1Brand.value);
-    var wheel2 = new Wheel(wheel2Diameter.valueAsNumber, wheel2Brand.value);
-    var wheel3 = new Wheel(wheel3Diameter.valueAsNumber, wheel3Brand.value);
-    var wheel4 = new Wheel(wheel4Diameter.valueAsNumber, wheel4Brand.value);
-    car.addWheel(wheel1);
-    car.addWheel(wheel2);
-    car.addWheel(wheel3);
-    car.addWheel(wheel4);
-    generateWheelsDisplay();
+    var wheelsDataValidation = wheelsRegisterValidation(wheel1Brand, wheel1Diameter, wheel2Brand, wheel2Diameter, wheel3Brand, wheel3Diameter, wheel4Brand, wheel4Diameter);
+    if (wheelsDataValidation) {
+        var wheel1 = new Wheel(wheel1Diameter.valueAsNumber, wheel1Brand.value);
+        var wheel2 = new Wheel(wheel2Diameter.valueAsNumber, wheel2Brand.value);
+        var wheel3 = new Wheel(wheel3Diameter.valueAsNumber, wheel3Brand.value);
+        var wheel4 = new Wheel(wheel4Diameter.valueAsNumber, wheel4Brand.value);
+        car.addWheel(wheel1);
+        car.addWheel(wheel2);
+        car.addWheel(wheel3);
+        car.addWheel(wheel4);
+        generateWheelsDisplay();
+    }
 }
+// DISPLAYS //
 function generateCarDisplay() {
     deleteCarContainerFromView();
     generateWheelsContainerForView();
@@ -34,6 +41,7 @@ function generateCarDisplay() {
 function generateWheelsDisplay() {
     generateWheelsTable();
 }
+//
 function deleteCarContainerFromView() {
     var carContainer = document.getElementById("car-container");
     carContainer.style.display = "none";
@@ -78,4 +86,122 @@ function generateWheelsTable() {
         cell3j.appendChild(text3j);
         wheelNumber++;
     }
+}
+// FORM VALIDATION //
+function carRegisterValidation(plate, brand, color) {
+    var form = document.getElementById('car-register');
+    var errorPlate = document.getElementById('errorPlate');
+    var errorBrand = document.getElementById('errorBrand');
+    var errorColor = document.getElementById('errorColor');
+    var errorAccount = 0;
+    form.classList.remove('is-invalid'); // ¿por qué se sigue viendo el error cuando vuleve a aplicar el método? 
+    if (plate.value == "") {
+        plate.classList.add("is-invalid");
+        errorPlate.textContent = "Plate is a required field";
+        errorAccount++;
+    }
+    else if (!validatePlate(plate.value)) {
+        plate.classList.add("is-invalid");
+        errorPlate.textContent = "Plate format error";
+        errorAccount++;
+    }
+    if (brand.value == "") {
+        brand.classList.add("is-invalid");
+        errorBrand.textContent = "Brand is a required field";
+        errorAccount++;
+    }
+    if (color.value == "") {
+        color.classList.add("is-invalid");
+        errorColor.textContent = "Color is a required field";
+        errorAccount++;
+    }
+    if (errorAccount > 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+function validatePlate(plate) {
+    var regex = /^[0-9]{4}[a-zA-Z]{3}$/;
+    return regex.test(plate) ? true : false;
+}
+function wheelsRegisterValidation(wheel1Brand, wheel1Diameter, wheel2Brand, wheel2Diameter, wheel3Brand, wheel3Diameter, wheel4Brand, wheel4Diameter) {
+    var form = document.getElementById('wheels-register');
+    var errorWheel1Brand = document.getElementById('errorWheel1Brand');
+    var errorWheel1Diameter = document.getElementById('errorWheel1Diameter');
+    var errorWheel2Brand = document.getElementById('errorWheel2Brand');
+    var errorWheel2Diameter = document.getElementById('errorWheel2Diameter');
+    var errorWheel3Brand = document.getElementById('errorWheel3Brand');
+    var errorWheel3Diameter = document.getElementById('errorWheel3Diameter');
+    var errorWheel4Brand = document.getElementById('errorWheel4Brand');
+    var errorWheel4Diameter = document.getElementById('errorWheel4Diameter');
+    var errorAccount = 0;
+    form.classList.remove("is-invalid");
+    if (wheel1Brand.value == "") {
+        wheel1Brand.classList.add("is-invalid");
+        errorWheel1Brand.textContent = "Brand is a required field";
+        errorAccount++;
+    }
+    if (!validateDiameter(Number(wheel1Diameter.value))) {
+        wheel1Diameter.classList.add("is-invalid");
+        errorWheel1Diameter.textContent = "Diameter must be between 0.4 and 2";
+        errorAccount++;
+    }
+    if (wheel2Brand.value == "") {
+        wheel2Brand.classList.add("is-invalid");
+        errorWheel2Brand.textContent = "Brand is a required field";
+        errorAccount++;
+    }
+    if (!validateDiameter(Number(wheel2Diameter.value))) {
+        wheel2Diameter.classList.add("is-invalid");
+        errorWheel2Diameter.textContent = "Diameter must be between 0.4 and 2";
+        errorAccount++;
+    }
+    if (wheel3Brand.value == "") {
+        wheel3Brand.classList.add("is-invalid");
+        errorWheel3Brand.textContent = "Brand is a required field";
+        errorAccount++;
+    }
+    if (!validateDiameter(Number(wheel3Diameter.value))) {
+        wheel3Diameter.classList.add("is-invalid");
+        errorWheel3Diameter.textContent = "Diameter must be between 0.4 and 2";
+        errorAccount++;
+    }
+    if (wheel4Brand.value == "") {
+        wheel4Brand.classList.add("is-invalid");
+        errorWheel4Brand.textContent = "Brand is a required field";
+        errorAccount++;
+    }
+    if (!validateDiameter(Number(wheel4Diameter.value))) {
+        wheel4Diameter.classList.add("is-invalid");
+        errorWheel4Diameter.textContent = "Diameter must be between 0.4 and 2";
+        errorAccount++;
+    }
+    if (errorAccount > 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+function validateDiameter(diameter) {
+    return diameter > 0.4 && diameter < 2 ? true : false;
+}
+// event listeners
+var carForm = document.getElementById('car-register');
+if (carForm) {
+    carForm.addEventListener('blur', function (event) {
+        console.log(event);
+        if (event.target.value != "")
+            event.target.classList.remove('is-invalid');
+    }, true);
+}
+var wheelsForm = document.getElementById('wheels-register');
+if (wheelsForm) {
+    wheelsForm.addEventListener('blur', function (event) {
+        console.log(event);
+        if (event.target.value != "")
+            event.target.classList.remove('is-invalid');
+    }, true);
 }
